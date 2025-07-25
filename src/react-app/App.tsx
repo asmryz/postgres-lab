656b7f06e07029597a22d21aacc2df4dfacee2de
Asim Riaz <asim.riaz@live.com>
2025-07-25 07:39:56 +0500

    Updates

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -4,123 +4,149 @@ import React, { Fragment, useEffect, useState } from "react";
 import "./App.css";
 
 function useStickyState(defaultValue: string, key: string) {
-  const [value, setValue] = React.useState(() => {
-    const stickyValue = window.localStorage.getItem(key)
-    return stickyValue !== null ? JSON.parse(stickyValue) : defaultValue
-  })
-  React.useEffect(() => {
-    window.localStorage.setItem(key, JSON.stringify(value))
-  }, [key, value])
-  return [value, setValue]
+	const [value, setValue] = React.useState(() => {
+		const stickyValue = window.localStorage.getItem(key);
+		return stickyValue !== null ? JSON.parse(stickyValue) : defaultValue;
+	});
+	React.useEffect(() => {
+		window.localStorage.setItem(key, JSON.stringify(value));
+	}, [key, value]);
+	return [value, setValue];
 }
-
 
 function App() {
 	const [query, setQuery] = useState("SELECT * FROM jobs");
 	const [result, setResult] = useState([]);
-	const [schema, setSchema] = useStickyState('public', 'schema')
- 
+	const [schema, setSchema] = useStickyState("", "schema");
+
+	// useEffect(() => {
+	//     setSchema(window.localStorage.getItem("schema")|| "public");
+	// }, []);
+
 	useEffect(() => {
-		fetch(`/execute/SET SCHEMA '${schema}';`).then((res) => res.json());
+		if (schema !== "") {
+			fetch(`/schema/${schema}`).then((res) => res.json());
+		}
 	}, [schema]);
 
 	const handleChange = async (e: React.ChangeEvent<HTMLTextAreaElement | HTMLSelectElement>) => {
 		const { name, value } = e.target;
-		if(name === "query") { setQuery(value) }
-		if(name === "schema") { 
-			setSchema(value) 
-		} 
+		if (name === "query") {
+			setQuery(value);
+		}
+		if (name === "schema") {
+			setSchema(value);
+			console.log(`SET SCHEMA '${value}';`);
+		}
 		//setQuery(value);
-		console.log(`name : ${name},  value : ${value}`);
+		// console.log(`name : ${name},  value : ${value}`);
 		// You can add logic to handle the query change here
 	};
 
 	const handleDoubleClick = async () => {
-		console.log("Double clicked on the textarea");
+		console.log("Executing Query ......");
+		console.log(query);
 		let response = await fetch(`/execute/${encodeURIComponent(query)}`).then((res) => res.json());
-		console.log(response);
+		//console.log(response);
 		setResult(response);
 		// You can add logic to handle double click here
 	};
 
 	return (
 		<>
-			<div style={{ position: "fixed", top: 0, left: 0, width: "100%", zIndex: 1000, textAlign: "left", background: "#fff" }}> 
-				Schema :
+			<div style={{ position: "fixed", top: 0, left: 0, width: "100%", zIndex: 1000, textAlign: "left", background: "#fff" }}>
+				Schema : {" "}
 				<select name="schema" value={schema} onChange={handleChange}>
+					<option hidden></option>
 					<option value="public">public</option>
 					<option value="hr">hr</option>
 					<option value="uni">uni</option>
 				</select>
 			</div>
-			<div style={{  position: "fixed", top: 30, left: 0,  width: "100%", zIndex: 1000, textAlign: "left", background: "#fff" }}>
-				<a
-					href="#"
-					onClick={handleDoubleClick}
-					style={{
-						display: "inline-block",
-						marginBottom: "0.25rem",
-						verticalAlign: "top",
-						marginRight: "0.5rem",
-						marginTop: "1rem"
-					}}
-				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						xmlnsXlink="http://www.w3.org/1999/xlink"
-						fill="#7d7d7d"
-						height="30px"
-						width="30px"
-						version="1.1"
-						id="Capa_1"
-						viewBox="0 0 52.00 52.00"
-						xmlSpace="preserve"
-						stroke="#7d7d7d"
-					>
-						<g id="SVGRepo_iconCarrier">
-							<g>
-								<path d="M45.563,29.174l-22-15c-0.307-0.208-0.703-0.231-1.031-0.058C22.205,14.289,22,14.629,22,15v30 c0,0.371,0.205,0.711,0.533,0.884C22.679,45.962,22.84,46,23,46c0.197,0,0.394-0.059,0.563-0.174l22-15 C45.836,30.64,46,30.331,46,30S45.836,29.36,45.563,29.174z M24,43.107V16.893L43.225,30L24,43.107z" />{" "}
-							</g>
-						</g>
-					</svg>
-				</a>
+			{schema !== "" && (
+				<>
+					<div style={{ position: "fixed", top: 30, left: 0, width: "100%", zIndex: 1000, textAlign: "left", background: "#fff" }}>
+						<a
+							href="#"
+							onClick={handleDoubleClick}
+							style={{
+								display: "inline-block",
+								marginBottom: "0.25rem",
+								verticalAlign: "top",
+								marginRight: "0.5rem",
+								marginTop: "1rem",
+							}}
+						>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								xmlnsXlink="http://www.w3.org/1999/xlink"
+								fill="#7d7d7d"
+								height="30px"
+								width="30px"
+								version="1.1"
+								id="Capa_1"
+								viewBox="0 0 52.00 52.00"
+								xmlSpace="preserve"
+								stroke="#7d7d7d"
+							>
+								<g id="SVGRepo_iconCarrier">
+									<g>
+										<path d="M45.563,29.174l-22-15c-0.307-0.208-0.703-0.231-1.031-0.058C22.205,14.289,22,14.629,22,15v30 c0,0.371,0.205,0.711,0.533,0.884C22.679,45.962,22.84,46,23,46c0.197,0,0.394-0.059,0.563-0.174l22-15 C45.836,30.64,46,30.331,46,30S45.836,29.36,45.563,29.174z M24,43.107V16.893L43.225,30L24,43.107z" />{" "}
+									</g>
+								</g>
+							</svg>
+						</a>
 
-				<textarea
-					name="query"
-					rows={4}
-					value={query}
-					onChange={handleChange}
-					onDoubleClick={handleDoubleClick}
-					style={{
-						top: 0,
-						left: 0,
-						width: "50%",
-						zIndex: 1001,
-						boxSizing: "border-box",
-						// fontSize: "1.1rem",
-						padding: "0.5rem",
-					}}
-				/>
-			</div>
+						<textarea
+							name="query"
+							rows={4}
+							value={query}
+							onChange={handleChange}
+							onDoubleClick={handleDoubleClick}
+							style={{
+								top: 0,
+								left: 0,
+								width: "50%",
+								zIndex: 1001,
+								boxSizing: "border-box",
+								// fontSize: "1.1rem",
+								padding: "0.5rem",
+							}}
+						/>
+					</div>
 
-			<div style={{ marginTop: "90px", height: "calc(100vh - 80px)", overflowY: "auto", width: "4000px" }}>
-				<table>
-					{result.map((item: any, index: number) => (
-						<Fragment key={index}>
-							<thead>
-								<tr>{index === 0 && Object.keys(item).map((key, idx) => <th key={idx}>{key}</th>)}</tr>
-							</thead>
-							<tbody>
-								<tr>
-									{Object.entries(item).map(([key], idx) => (
-										<td key={idx}>{typeof item[key] === "object" ? item[key] : item[key].toString()}</td>
-									))}
-								</tr>
-							</tbody>
-						</Fragment>
-					))}
-				</table>
-			</div>
+					<div style={{ marginTop: "90px", height: "calc(100vh - 80px)", overflowY: "auto", width: "4000px" }}>
+						{Array.isArray(result) && result.length > 0 ? (
+							<table>
+								{result.map((item: any, index: number) => (
+									<Fragment key={index}>
+										<thead>
+											<tr>
+												{index === 0 &&
+													Object.keys(item).map((key, idx) => (
+														<th key={idx} style={{ fontWeight: "bolder" }}>
+															{key}
+														</th>
+													))}
+											</tr>
+										</thead>
+										<tbody>
+											<tr>
+												{Object.entries(item).map(([key], idx) => (
+													<td key={idx}>{typeof item[key] === "object" ? item[key] : item[key].toString()}</td>
+												))}
+											</tr>
+										</tbody>
+									</Fragment>
+								))}
+							</table>
+						) : (
+							<div style={{ padding: "1rem", color: "#888" }}>{typeof result === "string" ? result : "No results or result is not an array."}</div>
+						)}
+					</div>
+				</>
+			)}
+			<pre style={{ textAlign: "left" }}>{JSON.stringify({ schema }, null, 4)}</pre>
 		</>
 	);
 }
--- src/worker/routes/index.ts
+++ src/worker/routes/index.ts
@@ -6,8 +6,21 @@ indexRoute.get("/api/", async (c) => c.json({ name: "Cloudflare" }));
 
 indexRoute.get("/execute/:query", async (c) => {
     console.log(c.req.param("query"));
+
 	try {
 		const result = await sql.query(c.req.param("query"));
+		//console.log(result);
+		return c.json(result, 200);
+	} catch (err) {
+		return c.json({ error: (err as Error).message }, 500);
+	}
+});
+
+indexRoute.get("/schema/:schema", async (c) => {
+    console.log(c.req.param("schema"));
+
+	try {
+		const result = await sql.query(`SET SCHEMA '${c.req.param("schema")}';`);
 		//console.log(result);
 		return c.json(result, 200);
 	} catch (err) {
